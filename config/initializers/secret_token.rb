d07725e62af6200b49636763bd828f2c0e6e7f07
Jonathan Chen <jxmoneyz@gmail.com>
2013-03-26 23:12:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calccare::Application.config.secret_token = 'db07fcba3719b84383ca48c52b811e774f098c0ba4679524f12fe414a4794c79e93ba9029cab37c3fc9708e22f9c71612aef812bb2d9ebe975ece07d0a3aa428'
