b1153c41dbf2d50bd0999c0a6c8053daa319f025
Jonathan Chen <jxmoneyz@gmail.com>
2013-03-31 23:59:50 -0400

    Fixed Added Calculation to database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,9 @@
 class StaticPagesController < ApplicationController
 	def home
 		@num = Calculation.find(1)
-		if @num = nil
+		if @num == nil
 			@num = Calculation.new
 			@num.num = 0
+		end
 	end
 end
